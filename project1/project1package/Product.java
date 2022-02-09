7684483fd1ac93eff04e265af3b51912a9beedd9
DakotaVarnell <jvarnell1@cub.uca.edu>
2022-02-09 12:28:11 -0600

    1st Commit

---
+++ project1/project1package/Product.java
@@ -0,0 +1,10 @@
+package project1package;
+
+public class Product {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ project1/project1package/ProductCollection.java
@@ -0,0 +1,10 @@
+package project1package;
+
+public class ProductCollection {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
